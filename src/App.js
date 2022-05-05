e1c335967ca7e2481e9b9d56b30537b0dd144535
DokeDoke1 <nyandarmen@mail.ru>
2022-05-06 00:26:24 +0500

    Darmens Commit First

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from './screens/Home/Home.js';
 
+
 function App() {
   return (
     <div className="App">
       <Home />
+      
     </div>
   );
 }
--- src/screens/Reviews/Reviews.js
+++ src/screens/Reviews/Reviews.js
@@ -1,9 +1,14 @@
+/* eslint-disable jsx-a11y/heading-has-content */
 import React from 'react'
+import './Reviews.css';
 
 export default function Reviews() {
   return (
     <div className="screen-container">
-      Reviews
+      <div className='Skin'>
+        <h1 className='ReviewTag'>Отзывы</h1> 
+        <h2 className='SecondTag'>Я стараюсь чтобы все мои клиенты остались довольными от моей работы</h2> 
+      </div>
     </div>
   );
 }
